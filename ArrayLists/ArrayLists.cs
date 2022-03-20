733d23c639e6293478b7042303c6d725e17ee927
baikovIlya <baikusha23@gmail.com>
2022-03-20 20:27:06 +0300

    Preparation

--- ArrayLists/ArrayLists.cs
+++ ArrayLists/ArrayLists.cs
@@ -6,7 +6,116 @@ using System.Threading.Tasks;
 
 namespace ArrayLists
 {
-    internal class Class1
+    public class ArrayLists
     {
+        public int Length { get; private set; }
+
+        private int[] _main_array;
+
+        public ArrayLists()
+        {
+            _main_array = new int[10];
+            Length = 0;
+        }
+
+        public ArrayLists(int element)
+        {
+            Length = 1;
+            _main_array = new int[5] { element, 0, 0, 0, 0 };
+        }
+
+        public ArrayLists(int[] array)
+        {
+            Length = array.Length;
+            _main_array = PlusSize(array);
+        }
+
+        public void AddLast(int last_elemet)
+        {
+            SizeCheck(_main_array);
+            _main_array[Length] = last_elemet;
+            Length++;
+        }
+
+        public void AddFirst(int first_element)
+        {
+            MoveRight();
+            _main_array[0] = first_element;
+            Length++;
+        }
+
+        public void AddByIndex(int new_element, int index)
+        {
+            MoveRight(index);
+            _main_array[index] = new_element;
+            Length++;
+        }
+
+
+
+
+
+
+
+
+
+        
+        private int[] PlusSize(int[] array)
+        {
+            int result_length = (int)(array.Length*1.5+1);
+            int[] result = new int[result_length];
+            for (int i = 0; i < result_length; i++)
+            {
+                if (i < array.Length)
+                {
+                    result[i] = array[i];
+                }
+                else
+                {
+                    result[i] = 0;
+                }
+            }    
+            return result;
+        }
+
+        private int[] MinusSize(int[] array)
+        {
+            int result_length = (int)(Length*1.5+1);
+            int[] result = new int[result_length];
+            for (int i = 0; i < result_length; i++)
+            {
+                result[i] = array[i];
+            }
+            return result;
+        }
+        
+        private void SizeCheck(int[] array)
+        {
+            if (_main_array.Length <= Length)
+            {
+                _main_array = PlusSize(_main_array);
+            }
+            else if (_main_array.Length > Length*2)
+            {
+                _main_array = MinusSize(_main_array);
+            }
+        }
+
+        private void MoveRight(int index = 0)
+        {
+            SizeCheck(_main_array);
+            int[] newArray = new int[_main_array.Length+1];
+            for (int i = 0; i < _main_array.Length; i++)
+            {
+                newArray[i] = _main_array[i];
+            }
+            for (int i = index; i<_main_array.Length; i++)
+            {
+                newArray[i+1] = _main_array[i];
+            }
+            newArray[index] = 0;
+            _main_array = newArray;
+        }
+
     }
 }
--- ArrayLists/Program.cs
+++ ArrayLists/Program.cs
@@ -1,2 +1,2 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = Convert.ToInt32(9*1.5);
+Console.WriteLine(a);
